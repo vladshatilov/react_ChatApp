59b57fd75bf7f8d95206988bc7fcf59dee7c0576
vladshatilov <shatilovvlad@mail.ru>
2020-08-11 15:37:40 +0300

    Updates

---
+++ precache-manifest.5578abf6bda3323883a4a96bb6fd5bab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a4294836acc9e7db00714253d17678",
+    "url": "/react_ChatApp/index.html"
+  },
+  {
+    "revision": "fe2cd13523f0265f01db",
+    "url": "/react_ChatApp/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "286b53dfd68e07316142",
+    "url": "/react_ChatApp/static/css/main.a52bba9d.chunk.css"
+  },
+  {
+    "revision": "fe2cd13523f0265f01db",
+    "url": "/react_ChatApp/static/js/2.357f38c7.chunk.js"
+  },
+  {
+    "revision": "437dc5e78cda866982f413ccad0e6955",
+    "url": "/react_ChatApp/static/js/2.357f38c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286b53dfd68e07316142",
+    "url": "/react_ChatApp/static/js/main.2b513dc5.chunk.js"
+  },
+  {
+    "revision": "58ad364f5ce0bbc67e01",
+    "url": "/react_ChatApp/static/js/runtime-main.5ff90a95.js"
+  },
+  {
+    "revision": "260d02e482776c5da781bef45990ebea",
+    "url": "/react_ChatApp/static/media/chat_icon.260d02e4.svg"
+  },
+  {
+    "revision": "81462e32841fb0d663b80cd3ce6ed659",
+    "url": "/react_ChatApp/static/media/img.81462e32.jpg"
+  }
+]);
